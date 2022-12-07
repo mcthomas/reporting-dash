4bb9335facffe12ef6905ebcb7ce51d1f0f63808
mcthomas <matt.thomas2@icloud.com>
2022-12-07 12:43:14 -0600

    Force default value for empty user report fields

--- app/models/userreport.rb
+++ app/models/userreport.rb
@@ -1,2 +1,14 @@
 class Userreport < ApplicationRecord
+    before_save :set_default
+
+    protected
+
+    def set_default
+      self.users = 0 unless self.users
+      self.usersactive = 0 unless self.usersactive
+      self.usersinactive = 0 unless self.usersinactive
+      self.usersvalidated = 0 unless self.usersvalidated
+      self.usersnotvalidated = 0 unless self.usersnotvalidated
+    end
+    
 end
