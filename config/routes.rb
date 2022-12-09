da225e7a852fc6278d2a9c925d93bf1f47d8f27b
mcthomas <matt.thomas2@icloud.com>
2022-12-09 14:54:45 -0600

    CSS for devise login views, route name tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  get 'manage' => 'users#adminconsole', :as => :manage
-  get 'channelplans/dashboard' => 'channelplans#dashboard', :as => :dashboard
+  get 'manage-users' => 'users#adminconsole', :as => :manage
+  get 'dashboard/manage' => 'channelplans#dashboard', :as => :dashboard
   resources :channelplans, :userreports, :elgibilitylists
   resources :eligibilitylists do
     collection do
